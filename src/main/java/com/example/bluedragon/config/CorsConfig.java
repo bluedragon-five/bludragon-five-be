bdc68658667179d46ddaacf7d9f64685ceaa1b46
coli <kkwoo001021@naver.com>
2025-02-09 01:10:10 +0900

    chore: Cors 설정

--- src/main/java/com/example/bluedragon/config/CorsConfig.java
+++ src/main/java/com/example/bluedragon/config/CorsConfig.java
@@ -1,5 +1,26 @@
 package com.example.bluedragon.config;
 
-public class CorsConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(new String[]{"http://localhost:5173/", "https://bluedragon-five-fe.vercel.app"})
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+                .allowCredentials(true)
+                .allowedHeaders("*");
+    }
 }
