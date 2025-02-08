2234cca0502a0229105ab80b60b54254d4bbdb96
abt576 <limut0913@gmail.com>
2025-02-08 22:02:40 +0900

    feat: 장학금 검색 method 구현

--- src/main/java/com/example/bluedragon/repository/ScholarshipRepository.java
+++ src/main/java/com/example/bluedragon/repository/ScholarshipRepository.java
@@ -1,8 +1,23 @@
 package com.example.bluedragon.repository;
 
+import com.example.bluedragon.domain.Major;
 import com.example.bluedragon.domain.Scholarship;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 public interface ScholarshipRepository extends JpaRepository<Scholarship, Long> {
-    
+
+    @Query("""
+       select s
+       from Scholarship as s
+       where s.section <= :section
+       and s.type = :type
+       and s.major = :major
+       and s.attendance = :attendance
+      """)
+    List<Scholarship> findByCondition(long section, Type type, Major major, boolean attendance, long grade, Sort sort);
 }
